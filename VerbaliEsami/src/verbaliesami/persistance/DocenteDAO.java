2f007a9a70d476bf29b8e2d138468600e8c10e74
Giuseppe <buonomanog@gmail.com>
2022-06-02 17:26:08 +0200

    Docente e DocenteDAO

---
+++ VerbaliEsami/src/verbaliesami/persistance/DocenteDAO.java
@@ -0,0 +1,139 @@
+package verbaliesami.persistance;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import verbaliesami.entity.Docente;
+
+public class DocenteDAO {
+
+	
+	public static void create(String nome, String cognome, String matricola, String username, String password) throws SQLException{
+		
+		PreparedStatement p = null;
+		
+		try {
+			Connection conn = DBManager.getInstance().getConnection();
+			p = conn.prepareStatement("INSERT INTO DOCENTE (matricola, cognome, nome, username, password) VALUES (?, ?, ?, ?, ?)");
+			p.setString(1, matricola);
+			p.setString(2, cognome);
+			p.setString(3, nome);
+			p.setString(4, username);
+			p.setString(5, password);
+			p.executeUpdate();
+			
+		} finally {
+			if (p != null) {
+				p.close();
+			}
+		}
+		
+		
+	}
+	
+	
+	public static void create(Docente d) throws SQLException {
+		
+		PreparedStatement p = null;
+		
+		try {
+			Connection conn = DBManager.getInstance().getConnection();
+			p = conn.prepareStatement("INSERT INTO DOCENTE (matricola, cognome, nome, username, password) VALUES (?, ?, ?, ?, ?)");
+			p.setString(1, d.getMatricola());
+			p.setString(2, d.getCognome());
+			p.setString(3, d.getNome());
+			p.setString(4, d.getUsername());
+			p.setString(5, d.getPassword());
+			p.executeUpdate();
+		
+		} finally {
+			if (p != null) {
+				p.close();
+			}
+		}
+		
+	}
+	
+	
+	public static void delete(String matricola) throws SQLException{
+		
+		PreparedStatement p = null;
+		
+		try {
+			Connection conn = DBManager.getInstance().getConnection();
+			p = conn.prepareStatement("DELETE FROM DOCENTE WHERE matricola=?");
+			p.setString(1, matricola);
+			p.executeUpdate();
+			
+		} finally {
+			if (p != null) {
+				p.close();
+			}
+		}
+	}
+	
+	
+	public static Docente readSafe(String matricola) throws SQLException{
+		
+		PreparedStatement p = null;
+		
+		try {
+		Connection conn = DBManager.getInstance().getConnection();
+		p = conn.prepareStatement("SELECT matricola, nome, cognome, username FROM DOCENTE WHERE matricola=?");
+		p.setString(1, matricola);
+		ResultSet rs = p.executeQuery();
+		Docente d = null;
+		
+		if (rs.next()) {
+			String m = rs.getString("matricola");
+			String n = rs.getString("nome");
+			String c = rs.getString("cognome");
+			String u = rs.getString("username");
+			d = new Docente (n, c, m, u, "****");
+			
+		}
+		return d;
+		
+		} finally {
+			if (p != null) {
+				p.close();
+			}
+		}
+	}
+		
+
+		public static Docente readUnsafe(String matricola) throws SQLException{
+			
+			PreparedStatement p = null;
+			
+			try {
+			Connection conn = DBManager.getInstance().getConnection();
+			p = conn.prepareStatement("SELECT matricola, nome, cognome, username, password FROM DOCENTE WHERE matricola=?");
+			p.setString(1, matricola);
+			ResultSet rs = p.executeQuery();
+			Docente d = null;
+			
+			if (rs.next()) {
+				String m = rs.getString("matricola");
+				String n = rs.getString("nome");
+				String c = rs.getString("cognome");
+				String u = rs.getString("username");
+				String pw = rs.getString("password");
+				d = new Docente (n, c, m, u, pw);
+				
+			}
+			return d;
+			
+			} finally {
+				if (p != null) {
+					p.close();
+				}
+			}		
+				
+	}
+	
+	
+	
+}
--- VerbaliEsami/src/verbaliesami/persistance/verbali_main.java
+++ VerbaliEsami/src/verbaliesami/persistance/verbali_main.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 //import verbaliesami.entity.Studente;
 import verbaliesami.entity.Corso;
 import verbaliesami.entity.Studente;
+import verbaliesami.entity.Docente;
 
 public class verbali_main {
 
@@ -21,7 +22,9 @@ public class verbali_main {
 		
 		//StudenteDAO.create("Aldo", "Baglio", "N46004951", "Ajeje", "Brazorf", 45620);
 		
-		StudenteDAO.create("Scemo", "Mongolo", "N46005050", "Attila", "Boh", 80809);
+		//StudenteDAO.create("Scemo", "Mongolo", "N46005050", "Attila", "Boh", 80809);
+		
+		DocenteDAO.create("gigi", "buffon", "N12345625", "gigione", "1234");
 		
 		Statement s = null;
 		try {
@@ -29,7 +32,7 @@ public class verbali_main {
 			ResultSet rs = s.executeQuery("SELECT * FROM STUDENTE");
 			while (rs.next()) {
 				String matricola = rs.getString("matricola");
-				System.out.println("SQL: " + matricola);
+			//	System.out.println("SQL: " + matricola);
 			}
 		} finally {
 			if (s != null) { s.close(); }
@@ -44,6 +47,38 @@ public class verbali_main {
 			ResultSet rs = s.executeQuery("SELECT * FROM STUDENTE");
 			while (rs.next()) {
 				String matricola = rs.getString("matricola");
+			//	System.out.println("SQL: " + matricola);
+			}
+		} finally {
+			if (s != null) { s.close(); }
+		}
+		
+		System.out.println("------------------");
+		try {
+			System.out.println("Prima di eseguire la cancellazione");
+
+			s = conn.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM DOCENTE");
+			while (rs.next()) {
+				String matricola = rs.getString("matricola");
+				System.out.println("SQL: " + matricola);
+			}
+		} finally {
+			if (s != null) { s.close(); }
+		}
+		
+		Docente d = DocenteDAO.readUnsafe("N12345625");
+		d.mostraInfoDocente();
+		DocenteDAO.delete("N12345625");
+		
+		System.out.println("------------------");
+		try {
+			System.out.println("Dopo aver eseguito la cancellazione");
+
+			s = conn.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM DOCENTE");
+			while (rs.next()) {
+				String matricola = rs.getString("matricola");
 				System.out.println("SQL: " + matricola);
 			}
 		} finally {
@@ -51,14 +86,15 @@ public class verbali_main {
 		}
 		
 		
+		
 		System.out.println("-----------------------");
 		
 		String matricola = "N46004818";
 		
-		System.out.println("Leggo Studente con matricola " + matricola);
-		Studente student = StudenteDAO.readSafe(matricola);
+		//System.out.println("Leggo Studente con matricola " + matricola);
+		Studente student = StudenteDAO.readUnsafe(matricola);
 		student.mostraInfoStudente();
-		System.out.println("--------------");
+		//System.out.println("--------------");
 		
 		//Corso c = CorsoDAO.create(4, "Sistemi operativi", 18);
 		
