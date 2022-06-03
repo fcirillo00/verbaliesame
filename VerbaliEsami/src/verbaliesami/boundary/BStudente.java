49b0518f6a74f64c1eacbb86700fb373242606b0
Francesco <francescocirillo2000@gmail.com>
2022-06-03 22:09:12 +0200

    Boundary Studente (TODO ancora)

--- VerbaliEsami/src/verbaliesami/boundary/BStudente.java
+++ VerbaliEsami/src/verbaliesami/boundary/BStudente.java
@@ -1,7 +1,9 @@
 package verbaliesami.boundary;
 
 import verbaliesami.control.VerbaliManagementSystem;
+import verbaliesami.entity.Appello;
 import verbaliesami.entity.Corso;
+import verbaliesami.persistance.AppelloDAO;
 import verbaliesami.persistance.CorsoDAO;
 
 import java.io.BufferedReader;
@@ -10,26 +12,57 @@ import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+class functions {
+	public static boolean yes () throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			boolean correct_input = true;
+			do {
+				String ans = br.readLine();
+				if (ans.compareToIgnoreCase("Y") == 0) {
+					return true;
+				} else if (ans.compareToIgnoreCase("N") == 0) {
+					return false;
+				} else {
+					System.out.println("Inserisci Y o N.");
+					correct_input = false;
+				}
+			} while (!correct_input);
+			
+			return false;
+	}
+}
 
 public class BStudente {
 	VerbaliManagementSystem control = VerbaliManagementSystem.getInstance();
 	
 	public static void prenota_appello() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		boolean corso_trovato = false;
+		boolean is_corso_trovato = false;
 		String pattern = "[0-9]*";
+		
+		Corso corso_scelto = null;
+		boolean completed = false;
 
 		do {
-			System.out.println("RICERCA CORSO");
-			System.out.println("Come vuoi cercare il corso?");
-			System.out.println("(1) Per nome");
-			System.out.println("(2) Per codice del corso");
-			System.out.println("(3) Per matricola del docente titolare");
-			System.out.println("(4) Per nome e cognome del docente titolare");
-			
-			try {
+		try {
+			do {
+				System.out.println("RICERCA CORSO");
+				System.out.println("Come vuoi cercare il corso?");
+				System.out.println("(1) Per nome");
+				System.out.println("(2) Per codice del corso");
+				System.out.println("(3) Per matricola del docente titolare");
+				System.out.println("(4) Per nome e cognome del docente titolare");
+				
+
 				System.out.flush();
-				int choice = Integer.valueOf(br.readLine());
+				String input;
+				do {
+					input = br.readLine();
+					if (!input.matches(pattern)) {
+						System.out.println("Inserire un numero.");
+					}
+				} while (!input.matches(pattern));
+				int choice = Integer.valueOf(input);
 				ArrayList<Corso> corsi = new ArrayList<Corso>();
 				switch(choice) {
 				case 1:
@@ -43,7 +76,6 @@ public class BStudente {
 					break;
 				case 2:
 					try {
-						String input;
 						do {
 							System.out.print("Inserisci il codice del corso: ");
 							System.out.flush();
@@ -98,18 +130,117 @@ public class BStudente {
 				}
 				
 				if (!corsi.isEmpty()) {
-					corso_trovato = true;
-					System.out.println("Corsi trovati.");
-					System.out.println(corsi.size());
-					System.out.println(corsi.get(0).getDenominazione());
+					System.out.println("Corsi trovati.\n");
+					// mostra corsi
+					for (int i=0; i<corsi.size(); i++) {
+						System.out.format("Corso (%d):\n", i+1);
+						corsi.get(i).mostraCorso();
+					}
+					
+					System.out.print("Hai trovato il corso che desideri? (Y/N): ");
+					if (functions.yes()) {
+						do {
+							System.out.println("Inserisci il numero del corso: ");
+							System.out.flush();
+							input = br.readLine();
+							if (!input.matches(pattern)) {
+								System.out.println("Inserisci un numero.");
+							} else if (! (Integer.valueOf(input) >= 1 && Integer.valueOf(input) <= corsi.size() )) {
+								System.out.println("Inserisci un numero compreso tra quelli mostrati.");
+							}
+						} while (!input.matches(pattern) && (Integer.valueOf(input) >= 1 && Integer.valueOf(input) <= corsi.size()));
+						
+						int scelta = Integer.valueOf(input);
+						corso_scelto = corsi.get(scelta-1);
+						is_corso_trovato = true;
+						
+					} else {
+						System.out.println("Ricerca annullata. Vuoi riprovare?");
+						if (!functions.yes()) {
+							return;
+						}
+					}
 				} else {
-					System.out.println("Corso non trovato. Riprova.");
+					System.out.println("Corso non trovato. Riprova. (se vuoi uscire premi Q)");
+					String ans = br.readLine();
+					if (ans.compareToIgnoreCase("Q") == 0) {
+						return;
+					}
 				}
-
-			} catch (IOException e) {
-				System.out.println("Input invalido");
+	
+			} while (!is_corso_trovato);
+		
+		
+			if (corso_scelto != null) {
+				System.out.println("Hai scelto "+corso_scelto.getDenominazione());
+				int codiceCorso = corso_scelto.getCodice();
+				ArrayList<Appello> appelli = new ArrayList<Appello>();
+				
+				try {
+					appelli = AppelloDAO.read(codiceCorso);
+				} catch (SQLException e) {
+					System.out.println("Errore nel DB.");
+				}
+				
+				if (!appelli.isEmpty()) {
+					Appello appello_scelto = null;
+					for (int i=0; i<appelli.size(); i++) {
+						System.out.format("Appello (%d): \n");
+						appelli.get(i).mostraInfoAppello();
+					}
+					
+					System.out.print("Hai trovato l'appello che desideri? (Y/N): ");
+					
+					if (functions.yes()) {
+						String input;
+						do {
+							System.out.println("Inserisci il numero dell'appello: ");
+							System.out.flush();
+							input = br.readLine();
+							if (!input.matches(pattern)) {
+								System.out.println("Inserisci un numero.");
+							} else if (! (Integer.valueOf(input) >= 1 && Integer.valueOf(input) <= appelli.size() )) {
+								System.out.println("Inserisci un numero compreso tra quelli mostrati.");
+							}
+						} while (!input.matches(pattern) && Integer.valueOf(input) >= 1 && Integer.valueOf(input) <= appelli.size());
+						
+						int scelta = Integer.valueOf(input);
+						appello_scelto = appelli.get(scelta-1);
+						
+						System.out.println("Hai scelto: ");
+						appello_scelto.mostraInfoAppello();
+						System.out.println("Vuoi confermare? (Y/N): ");
+						if (functions.yes()) {
+							// TODO
+							//prenota_appello(appello_scelto, )
+							completed = true;
+						} else {
+							
+						}
+						
+					} else {
+						System.out.println("Ricerca annullata. Vuoi riprovare?");
+						if (!functions.yes()) {
+							return;
+						}
+					}
+				} else {
+					System.out.println("Non ci sono appelli per il corso selezionato.");
+					System.out.print("Vuoi selezionare un altro corso? ");
+					if (!functions.yes()) {
+						completed = true;
+					}
+				}
+				
+			} else {
+				System.out.println("Errore scelta corso.");
 				return;
 			}
-		} while (!corso_trovato);
+			
+		} catch (IOException e) {
+				System.out.println("Input invalido");
+				return;
+		}
+		} while (!completed);
 	}
 }
