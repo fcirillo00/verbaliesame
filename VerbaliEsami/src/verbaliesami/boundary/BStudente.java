23da56d507793d4062ce792d9b47528ee64be41e
Francesco <francescocirillo2000@gmail.com>
2022-06-04 13:00:08 +0200

    ASDASD

--- VerbaliEsami/src/verbaliesami/boundary/BStudente.java
+++ VerbaliEsami/src/verbaliesami/boundary/BStudente.java
@@ -209,7 +209,6 @@ public class BStudente {
 								vms.prenota_appello(appello_scelto, studente_loggato);
 								completed = true;
 								System.out.println("Ti sei prenotato con successo.");
-								
 								break;
 							} else {
 								System.out.println("Prenotazione annullata. Vuoi scegliere un altro appello? (Y/N): ");
