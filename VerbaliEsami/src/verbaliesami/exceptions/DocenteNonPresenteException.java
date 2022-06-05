6edde311c5a5849266cc6fd0929a85c0af79ea0f
KaminariManu <93709785+KaminariManu@users.noreply.github.com>
2022-06-05 16:24:18 +0200

    DocenteNonPresenteException

---
+++ VerbaliEsami/src/verbaliesami/exceptions/DocenteNonPresenteException.java
@@ -0,0 +1,14 @@
+package verbaliesami.exceptions;
+
+public class DocenteNonPresenteException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8620579121336274845L;
+	
+	public DocenteNonPresenteException(String messaggio) {
+		super(messaggio);
+	}
+
+}
